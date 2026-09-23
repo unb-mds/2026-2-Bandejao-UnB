f68083afab63453a4b76f307599ce34e121c4c6f
Poviovsky <cristianopovoas7@gmail.com>
2026-09-22 22:02:06 -0300

    docs: correção para remover comentários no URL.py

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -6,7 +6,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('apps.status.urls')), 
     
-    # Minhas rotas da Sprint 2:
     path('api/cadastro/', usuarios_views.cadastro_view, name='api-cadastro'),
     path('api/login/', usuarios_views.login_view, name='api-login'),
 ]
